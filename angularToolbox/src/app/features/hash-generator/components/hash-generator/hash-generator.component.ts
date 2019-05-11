1942be27b08c4a07457a5ac6036df4f4b07b8411
federicok <phazedclient@gmail.com>
2019-05-11 20:07:47 +0200

    hash generator started, navigation changed

--- angularToolbox/src/app/features/hash-generator/components/hash-generator/hash-generator.component.ts
+++ angularToolbox/src/app/features/hash-generator/components/hash-generator/hash-generator.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+// IMPORTS FOR HASHING LIBRARIES
+import * as sha512 from "js-sha512";
+import { HashGenerator } from "../../models/hash-generator";
+//import sha256 from 'crypto-js/sha256';
+//import hmacSHA512 from 'crypto-js/hmac-sha512';
 
 @Component({
-  selector: 'app-hash-generator',
-  templateUrl: './hash-generator.component.html',
-  styleUrls: ['./hash-generator.component.scss']
+  selector: "app-hash-generator",
+  templateUrl: "./hash-generator.component.html",
+  styleUrls: ["./hash-generator.component.scss"]
 })
 export class HashGeneratorComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  createHash() {
+    console.log("createHash working");
+
+    var hashModel = new HashGenerator();
+
+    //var hash = sha512.sha512(password);
+
+    //hashModel.hash = hash;
   }
-
 }
--- angularToolbox/src/app/features/hash-generator/models/hash-generator.ts
+++ angularToolbox/src/app/features/hash-generator/models/hash-generator.ts
@@ -1,2 +1,4 @@
 export class HashGenerator {
+  password: String;
+  hash: String;
 }
