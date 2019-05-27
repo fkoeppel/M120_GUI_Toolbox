1970496382c2cf217dd0b0248a82ca2d98fa0e72
Kimi Janshon <kimi@kadenpartner.ch>
2019-05-27 13:46:19 +0200

    Pythagoras hinzugefügt

--- angularToolbox/src/app/app-routing.module.ts
+++ angularToolbox/src/app/app-routing.module.ts
@@ -5,17 +5,19 @@ import { HomeComponent } from './features/home/components/home/home.component';
 import { MetricUnitsComponent } from './features/metric-units/components/metric-units/metric-units.component';
 import { CoordinatesVisualizerComponent } from './features/coordinates-visualizer/components/coordinates-visualizer/coordinates-visualizer.component';
 import { HashGeneratorComponent } from './features/hash-generator/components/hash-generator/hash-generator.component';
+import { PythagorasComponent } from './features/pythagoras/components/pythagoras/pythagoras.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'breathalyzer', component: BreathalyzerComponent },
-  { path: 'metric-units', component: MetricUnitsComponent },
-  { path: 'coordinates-visualizer', component: CoordinatesVisualizerComponent },
-  { path: 'hash-generator', component: HashGeneratorComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'breathalyzer', component: BreathalyzerComponent },
+    { path: 'metric-units', component: MetricUnitsComponent },
+    { path: 'coordinates-visualizer', component: CoordinatesVisualizerComponent },
+    { path: 'hash-generator', component: HashGeneratorComponent },
+    { path: 'pythagoras', component: PythagorasComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {}
