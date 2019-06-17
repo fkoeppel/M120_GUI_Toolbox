64063b8facd248247ab63cb72383aca82c16cc49
byCrookie <maeni.h01@hotmail.com>
2019-06-17 15:31:54 +0200

    styles

--- angularToolbox/src/app/app.component.ts
+++ angularToolbox/src/app/app.component.ts
@@ -2,15 +2,22 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'angularToolbox';
+  title = 'angularToolbox';
 
-    routes = ['home', 'coordinates-visualizer', 'breathalyzer', 'hash-generator', 'metric-units', 'pythagoras'];
-    constructor(private router: Router) {
-        this.router.navigate([this.routes[5]]);
-    }
+  routes = [
+    'home',
+    'coordinates-visualizer',
+    'breathalyzer',
+    'hash-generator',
+    'metric-units',
+    'pythagoras'
+  ];
+  constructor(private router: Router) {
+    this.router.navigate([this.routes[0]]);
+  }
 }
